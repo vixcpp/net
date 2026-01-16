ae15113f8913d11e3aa22acb88ff74a9f1b53f33
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-01-16 18:04:40 +0300

    refactor

--- include/vix/net/NetworkProbe.hpp
+++ include/vix/net/NetworkProbe.hpp
@@ -1,4 +1,17 @@
-#pragma once
+/**
+ *
+ *  @file NetworkProbe.hpp
+ *  @author Gaspard Kirira
+ *
+ *  Copyright 2025, Gaspard Kirira.  All rights reserved.
+ *  https://github.com/vixcpp/vix
+ *  Use of this source code is governed by a MIT license
+ *  that can be found in the License file.
+ *
+ *  Vix.cpp
+ */
+#ifndef VIX_NETWORK_PROBE_HPP
+#define VIX_NETWORK_PROBE_HPP
 
 #include <cstdint>
 #include <functional>
@@ -6,35 +19,37 @@
 namespace vix::net
 {
 
-    class NetworkProbe
+  class NetworkProbe
+  {
+  public:
+    using ProbeFn = std::function<bool()>; // returns true if online
+
+    struct Config
     {
-    public:
-        using ProbeFn = std::function<bool()>; // returns true if online
-
-        struct Config
-        {
-            std::int64_t min_interval_ms{1000};
-            std::int64_t online_ttl_ms{2000}; // cached online validity
-            std::int64_t offline_ttl_ms{500}; // cached offline validity
-        };
-
-        NetworkProbe(Config cfg, ProbeFn fn);
-
-        bool isOnline(std::int64_t now_ms) const;
-        bool refresh(std::int64_t now_ms);
-        bool lastKnownOnline() const noexcept { return last_online_; }
-        std::int64_t lastProbeAtMs() const noexcept { return last_probe_at_ms_; }
-
-    private:
-        bool canProbe(std::int64_t now_ms) const noexcept;
-
-    private:
-        Config cfg_;
-        ProbeFn probe_;
-
-        bool last_online_{false};
-        std::int64_t last_probe_at_ms_{0};
-        std::int64_t last_update_ms_{0};
+      std::int64_t min_interval_ms{1000};
+      std::int64_t online_ttl_ms{2000}; // cached online validity
+      std::int64_t offline_ttl_ms{500}; // cached offline validity
     };
 
+    NetworkProbe(Config cfg, ProbeFn fn);
+
+    bool isOnline(std::int64_t now_ms) const;
+    bool refresh(std::int64_t now_ms);
+    bool lastKnownOnline() const noexcept { return last_online_; }
+    std::int64_t lastProbeAtMs() const noexcept { return last_probe_at_ms_; }
+
+  private:
+    bool canProbe(std::int64_t now_ms) const noexcept;
+
+  private:
+    Config cfg_;
+    ProbeFn probe_;
+
+    bool last_online_{false};
+    std::int64_t last_probe_at_ms_{0};
+    std::int64_t last_update_ms_{0};
+  };
+
 } // namespace vix::net
+
+#endif
--- src/NetworkProbe.cpp
+++ src/NetworkProbe.cpp
@@ -1,52 +1,64 @@
+/**
+ *
+ *  @file NetworkProbe.cpp
+ *  @author Gaspard Kirira
+ *
+ *  Copyright 2025, Gaspard Kirira.  All rights reserved.
+ *  https://github.com/vixcpp/vix
+ *  Use of this source code is governed by a MIT license
+ *  that can be found in the License file.
+ *
+ *  Vix.cpp
+ */
 #include <vix/net/NetworkProbe.hpp>
 
 namespace vix::net
 {
 
-    NetworkProbe::NetworkProbe(Config cfg, ProbeFn fn)
-        : cfg_(cfg), probe_(std::move(fn))
+  NetworkProbe::NetworkProbe(Config cfg, ProbeFn fn)
+      : cfg_(cfg), probe_(std::move(fn))
+  {
+  }
+
+  bool NetworkProbe::canProbe(std::int64_t now_ms) const noexcept
+  {
+    return (now_ms - last_probe_at_ms_) >= cfg_.min_interval_ms;
+  }
+
+  bool NetworkProbe::isOnline(std::int64_t now_ms) const
+  {
+    const auto age = now_ms - last_update_ms_;
+    const auto ttl = last_online_ ? cfg_.online_ttl_ms : cfg_.offline_ttl_ms;
+
+    if (age <= ttl)
     {
+      return last_online_;
+    }
+    // cache expired -> caller should call refresh()
+    return last_online_;
+  }
+
+  bool NetworkProbe::refresh(std::int64_t now_ms)
+  {
+    if (!probe_)
+    {
+      // No probe function -> assume online (or false). Choose conservative?
+      last_online_ = true;
+      last_update_ms_ = now_ms;
+      return last_online_;
     }
 
-    bool NetworkProbe::canProbe(std::int64_t now_ms) const noexcept
+    if (!canProbe(now_ms))
     {
-        return (now_ms - last_probe_at_ms_) >= cfg_.min_interval_ms;
+      // Too soon: return cached
+      return last_online_;
     }
 
-    bool NetworkProbe::isOnline(std::int64_t now_ms) const
-    {
-        const auto age = now_ms - last_update_ms_;
-        const auto ttl = last_online_ ? cfg_.online_ttl_ms : cfg_.offline_ttl_ms;
-
-        if (age <= ttl)
-        {
-            return last_online_;
-        }
-        // cache expired -> caller should call refresh()
-        return last_online_;
-    }
-
-    bool NetworkProbe::refresh(std::int64_t now_ms)
-    {
-        if (!probe_)
-        {
-            // No probe function -> assume online (or false). Choose conservative?
-            last_online_ = true;
-            last_update_ms_ = now_ms;
-            return last_online_;
-        }
-
-        if (!canProbe(now_ms))
-        {
-            // Too soon: return cached
-            return last_online_;
-        }
-
-        last_probe_at_ms_ = now_ms;
-        const bool ok = probe_();
-        last_online_ = ok;
-        last_update_ms_ = now_ms;
-        return last_online_;
-    }
+    last_probe_at_ms_ = now_ms;
+    const bool ok = probe_();
+    last_online_ = ok;
+    last_update_ms_ = now_ms;
+    return last_online_;
+  }
 
 } // namespace vix::net
